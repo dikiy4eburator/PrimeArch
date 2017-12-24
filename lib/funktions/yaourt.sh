5ddaf15c5d768bf4571e1a8efea5bbb520f45b3d
Unknown <dikiy4eburator@googlemail.com>
2017-12-24 16:01:29 +0100

    code cosmetic

--- lib/funktions/yaourt.sh
+++ lib/funktions/yaourt.sh
@@ -2,15 +2,6 @@
 
 cd /tmp
 
-
-#curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-#tar -xvzf package-query.tar.gz
-
-#curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
-#tar -xvzf yaourt.tar.gz
-
-
-
 git clone https://aur.archlinux.org/package-query.git
 git clone https://aur.archlinux.org/yaourt.git
 
